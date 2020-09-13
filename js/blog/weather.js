fdcd301d9ceb7699f41f0907fc442a2dbf64d574
DSzhongweizi <42842401+DSzhongweizi@users.noreply.github.com>
2020-09-13 19:59:30 +0800

    Create weather.js

---
+++ js/blog/weather.js
@@ -0,0 +1,11 @@
+WIDGET = {
+    CONFIG: {
+        "layout": 1,
+        "width": "450",
+        "height": "150",
+        "background": 1,
+        "dataColor": "FFFFFF",
+        "borderRadius": 5,
+        "key": "hkD2qemNXo"
+    }
+}
