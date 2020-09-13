c7e8c76050dcd33abb2096e923acc4dc23ae852e
DSzhongweizi <42842401+DSzhongweizi@users.noreply.github.com>
2020-09-13 21:50:20 +0800

    Update weather.js

--- js/blog/weather.js
+++ js/blog/weather.js
@@ -1,8 +1,6 @@
 WIDGET = {
     CONFIG: {
         "layout": 2,
-        "width": "230",
-        "height": "270",
         "background": 1,
         "dataColor": "FFFFFF",
         "borderRadius": 5,
