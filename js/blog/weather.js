de12f8c043a721313c76f9f164c502a359aaa464
DSzhongweizi <42842401+DSzhongweizi@users.noreply.github.com>
2020-09-13 22:43:01 +0800

    Update weather.js

--- js/blog/weather.js
+++ js/blog/weather.js
@@ -1,11 +1,11 @@
 WIDGET = {
-    CONFIG: {
-        "layout": 2,
-        "width": "230",
-        "height": "290",
-        "background": 1,
-        "dataColor": "FFFFFF",
-        "borderRadius": 5,
-        "key": "BmGKQ7ol5c"
-    }
+  CONFIG: {
+    "layout": 2,
+    "width": "230",
+    "height": "290",
+    "background": 1,
+    "dataColor": "FFFFFF",
+    "borderRadius": 5,
+    "key": "PnNYCSIrJp"
+  }
 }
