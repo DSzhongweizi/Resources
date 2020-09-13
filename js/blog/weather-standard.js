443b2d81db59438bdb5786c5eda28e720d1faf75
DSzhongweizi <42842401+DSzhongweizi@users.noreply.github.com>
2020-09-14 00:05:27 +0800

    Update weather-standard.js

--- js/blog/weather-standard.js
+++ js/blog/weather-standard.js
@@ -1,11 +1,11 @@
 WIDGET = {
-  CONFIG: {
-    "layout": 2,
-    "width": "230",
-    "height": "290",
-    "background": 1,
-    "dataColor": "FFFFFF",
-    "borderRadius": 5,
-    "key": "PnNYCSIrJp"
-  }
+    CONFIG: {
+        "layout": 2,
+        "width": "230",
+        "height": "290",
+        "background": 1,
+        "dataColor": "FFFFFF",
+        "borderRadius": 5,
+        "key": "xnXO8FFQLJ"
+    }
 }
